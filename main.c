a7b51c41d58156edf47493c16e4324daaa101e01
AngryMajor <peterfmajor@gmail.com>
2017-03-31 11:08:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,70 @@
+/*
+ * main.c
 
+ *
+ *  Created on: 29 Mar 2017
+ *      Author: liliana
+ */
+
+
+#include <stdio.h>
+#include "crossfireOperations.h"
+
+
+
+int main(){
+
+	int row, column, boardSize;
+
+	//pointer to slot (0,0)
+	struct slot *upLeft;
+
+	//pointer to slot (0,boardSize -1)
+	struct slot *upRight;
+
+	//pointer to slot (boardSize - 1, 0)
+	struct slot *downLeft;
+
+	//pointer to slot (boardSize - 1, boardSize -1)
+	struct slot *downRight;
+
+	//Obtains the board size from the user input
+	boardSize = getBoardSize();
+
+	//Creates the board
+	createBoard(boardSize,&upLeft, &upRight, &downLeft, &downRight);
+
+	/*	Asks the user the row and the column of the slot
+		s/he wants to retrieve from the board.
+		Note that the user needs to know the size of the board to input
+		the correct row and column of the slot s/he wants to retrieve */
+	getDesiredElement(boardSize, &row,&column);
+
+	//Finds the slot
+
+
+	/*If the the required slot is closer to the down-right
+	 * corner of the board the search starts from downRight,
+	 * which points to slot at position (boardSize-1, boarSize -1)*/
+	if(row >= boardSize/2){
+		if(column >= boardSize/2)
+			reachDesiredElement(row,column,downRight);
+		else
+		/*If the the required slot is closer to the down-left
+		* corner of the board the search starts from downLeft,
+		* which points to slot at position (boardSize-1, 0)*/
+			reachDesiredElement(row,column,downLeft);
+	} else {
+		/*If the the required slot is closer to the up-right
+		* corner of the board the search starts from upRight,
+		* which points to slot at position (0, boarSize -1)*/
+		if(column >= boardSize/2)
+			reachDesiredElement(row,column, upRight);
+		/*If the the required slot is closer to the up-left
+		* corner of the board the search starts from upLeft,
+		* which points to slot at position (0, 0)*/
+		else reachDesiredElement(row,column,upLeft);
+	}
+
+	return 0;
+}
