f2d652f3a552a535ef632f62cc47a0906519de0b
mgrogan0058 <matthew.grogan@ucdconnect.ie>
2017-04-01 21:41:25 +0100

    Update main.c

--- main.c
+++ main.c
@@ -56,6 +56,47 @@ int main(){
 		* which points to slot at position (0, 0)*/
 		else reachDesiredElement(row,column,upLeft);
 	}
-
-	return 0;
+int choice;  //Number to be entered by user
+	printf("Please input how many players are playing the game: ");  //Prompt user to enter a number
+	scanf("%d", &choice);  //Reads an integer used to represent the number of players playing the game
+	struct player player[choice];  
+	
+	for(int i = 0; i < choice; i++)  //Loop amount of times user inputed for choice
+	{
+		puts("There are four player options to choose from.");
+		puts("Enter the number corresponding to the player option.");
+		printf("1. Elf.\n2. Human.\n3. Ogre.\n4. Wizard\n");
+		
+		do{
+			printf("Please enter the number corresponding to the type for this player: ");
+			scanf("%d", &player[i].type);
+			switch(player[i].type)
+			{
+				case 1:
+				elf(&player[i]); //Function call
+				Joined_Players++;
+				break;
+				case 2:
+				human(&player[i]);  //Function call
+				Joined_Players++;
+				break;
+				case 3:
+				ogre(&player[i]);  //Function call
+				Joined_Players++;
+				break;
+				case 4:
+				wizard(&player[i]);  //Function call
+				Joined_Players++;
+				break;
+				default:
+				printf("That's not a valid option.\n");
+				}
+		  } while(player[i].type >4 || player[i].type <= 0); //End while
+	} 
+	if(Joined_Players < 2)
+	{
+		printf("\nSorry, there is not enough players to play the game.\n");
+		return main();
+	}
+return 0;
 }
