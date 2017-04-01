d7d2aedd3c160559279b144e2104540b9c9cddd4
mgrogan0058 <matthew.grogan@ucdconnect.ie>
2017-04-01 21:40:01 +0100

    Create Functions.h

---
+++ Functions.h
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <time.h>
+
+static int Joined_Players = 0;
+
+void elf(struct player *Current_Player);  //Function prototype
+void human(struct player *Current_Player);   //Function prototype
+void ogre(struct player *Current_Player);   //Function prototype
+void wizard(struct player *Current_Player);   //Function prototype 
+struct player type();  //Function prototype
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
